16d72850d347ba9f681be2af594a3c692f0d009f
jose Tovar <jose.tovar102397@gmail.com>
2021-09-27 14:56:45 -0600

    Update board sections in game.js

--- game.js
+++ game.js
@@ -4,19 +4,12 @@ class Game {
     this.player2 = player2;
     this.turn = player1;
     this.draw = false;
-    this.boardSections = ['', '', '', '', '', '', '', ''];
-    this.winConditions = [
-      [0, 1, 2], [3, 4, 5],
-      [6, 7, 8], [0, 4, 8],
-      [2, 4, 6], [0, 3, 6],
-      [1, 4, 7], [2, 5, 8]
-    ];
+    this.boardSections = ['', '', '', '', '', '', '', '', ''];
   }
 
   switchTurns() {
     if (this.turn === player1) {
       this.turn = player2;
-      playerTurn.innerText = `It's ${this.turn.token} turn!`
     } else {
       this.turn = player1;
       playerTurn.innerText = `It's ${this.turn.token} turn!`
