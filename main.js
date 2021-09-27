d83bbffe62ab964b1d051a53e7c0da6d4b67f306
jose Tovar <jose.tovar102397@gmail.com>
2021-09-27 15:42:36 -0600

    Apply game logic to display in DOM

--- main.js
+++ main.js
@@ -35,4 +35,47 @@ function addToken() {
     currentGame.switchTurns();
     showTurn();
   }
+  declareWinner();
+};
+
+function declareWinner() {
+  currentGame.checkForWinner();
+  if (currentGame.player1.winner) {
+    winner.innerText = `${currentGame.player1.token} wins!`;
+    hide(playerTurn);
+    show(winner);
+    currentGame.player1.saveWinsToStorage();
+    setTimeout(resetBoard, 1500);
+  } else if (currentGame.player2.winner) {
+    winner.innerText = `${currentGame.player2.token} wins!`;
+    hide(playerTurn);
+    show(winner);
+    currentGame.player2.saveWinsToStorage();
+    setTimeout(resetBoard, 1500);
+  }
+};
+
+function displayWins() {
+  player1Wins.innerText = currentGame.player1.retrieveWinsFromStorage();
+  player2wins.innerText = currentGame.player2.retrieveWinsFromStorage();
+};
+
+function resetBoard() {
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].innerText = '';
+  }
+  currentGame.player1.winner = false;
+  currentGame.player2.winner = false;
+  currentGame.boardSections = ['', '', '', '', '', '', '', '', ''];
+  hide(winner);
+  show(playerTurn);
+  displayWins();
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
+
+function hide(element) {
+  element.classList.add('hidden');
 };
